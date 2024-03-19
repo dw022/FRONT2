8c7acf6ab63055ac9f131437b9634d25c491b0ae
leedawoon <acebasket42@gmail.com>
2024-03-19 21:38:51 +0900

    씨드 데이터 생성 및 동기화 처리, 앱 처음 사용 시 씨드 데이터 생성

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Todo from "./components/Todo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 씨드 데이터 (샘플 데이터)
+const SEED = [
+  { id: "todo-0", name: "운동하기", completed: true},
+  { id: "todo-1", name: "춤 연습하기", completed: true},
+  { id: "todo-2", name: "밥 먹기", completed: false},
+]
+
+// 로컬스토리지 동기화 처리
+function saveData(tasks) {
+  localStorage.setItem("tasksData", JSON.stringify(tasks));
 }
 
-export default App;
+// 앱을 처음 사용하는 경우 씨드데이터를 생성한다
+if (!localStorage.getItem("tasksData")) {
+  saveData(SEED);
+}
+
+export default function App() {}
